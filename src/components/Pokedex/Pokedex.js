a22e8e744504c90b3fadc7c8e01cb576e1a7dffd
crepechoco57 <sarah.herberichs@neuf.fr>
2024-02-01 11:50:41 +0100

    0102241150

--- src/components/Pokedex/Pokedex.js
+++ src/components/Pokedex/Pokedex.js
@@ -1,24 +1,39 @@
-import {useContext} from "react";
+import { useContext, useState } from "react";
 import PokemonContext from "../../data/PokemonContext";
 import PokedexItem from "../PokedexItem/PokedexItem";
-import './pokedex.scss'
+import "./pokedex.scss";
 const Pokedex = () => {
+  const [state, dispatch] = useContext(PokemonContext);
+  const [searchTerm, setSearchTerm] = useState("");
+  const filteredPokemonsName = state.pokemons.filter((pokemon) =>
+    pokemon.name.toLowerCase().startsWith(searchTerm.toLowerCase())
+  );
+  // On récupère le state et le dispatch dans le context fourni par le Provider
 
-    // On récupère le state et le dispatch dans le context fourni par le Provider
-    const [state, dispatch] = useContext(PokemonContext)
-
-    // On retourne le template qui map sur la liste de pokemons contenu dans le state
-    // Et on affiche un composant PokedexItem pour chacun d'entre eux en lui fournissant le pokemon
-    // qu'il doit afficher grace à une props
-    return (
-        <section id="pokedex">
-            <h2>Pokedex</h2>
-            <div className="pokemon-list">
-                { state.pokemons.length > 0 && state.pokemons.map( (pokemon, i) => (
-                    <PokedexItem key={i} pokemon={pokemon}/>
-                ))}
-            </div>
-        </section>
-    )
-}
+  // On retourne le template qui map sur la liste de pokemons contenu dans le state
+  // Et on affiche un composant PokedexItem pour chacun d'entre eux en lui fournissant le pokemon
+  // qu'il doit afficher grace à une props
+  return (
+    <section id="pokedex">
+      <h2>Pokedex</h2>
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Search Pokemon..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+      <div className="pokemon-list">
+        {filteredPokemonsName.length > 0 ? (
+          filteredPokemonsName.map((pokemon, i) => (
+            <PokedexItem key={i} pokemon={pokemon} />
+          ))
+        ) : (
+          <p>No Pokemon found.</p>
+        )}
+      </div>
+    </section>
+  );
+};
 export default Pokedex;
--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -1,17 +1,31 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import PokemonContext from "../../data/PokemonContext";
 import "./team.scss";
+import { useLocation } from "react-router-dom";
 
 import BtnDetails from "../BtnDetails/BtnDetails";
 const Team = () => {
   // On recupere le state et le dispatch grace au context fourni par le Provider
   const [state, dispatch] = useContext(PokemonContext);
-
+  const location = useLocation();
+  const isOnTeamPage = location.pathname === "/team";
   // On déclare la fonction à executer au click sur le bouton "remove"
   const removeFromTeam = (index) => {
     // On demande au reducer d'utiliser le switch case qui supprime le pokemon de l'equipe
     // ( celui qui se trouve à l'index donné )
     dispatch({ type: "REMOVE_FROM_TEAM", payload: index });
+  };
+
+  const [hoveredPokemon, setHoveredPokemon] = useState(null);
+  const [isHovered, setIsHovered] = useState(null);
+
+  const handlePokemonHover = (pokemon) => {
+    setHoveredPokemon(pokemon);
+    setIsHovered(true);
+  };
+  const handleMouseOut = () => {
+    setHoveredPokemon(null);
+    setIsHovered(false);
   };
 
   return (
@@ -21,17 +35,48 @@ const Team = () => {
         {state.myTeam.length === 0 && <p>Team is empty.</p>}
         {state.myTeam.length > 0 &&
           state.myTeam.map((pokemon, i) => (
-            <article key={i}>
+            <article
+              key={i}
+              onMouseOver={() => handlePokemonHover(pokemon)}
+              onMouseOut={handleMouseOut}
+            >
               <img src={pokemon.sprites.front_default} alt={pokemon.name} />
               <div className="name-and-btn">
                 <span>{pokemon.name}</span>
-                <BtnDetails pokemonName={pokemon.name} />
-                <button
-                  className="btn-remove"
-                  onClick={() => removeFromTeam(i)}
-                >
-                  remove
-                </button>
+                {isOnTeamPage && hoveredPokemon === pokemon && (
+                  <div
+                    className={`hovered-pokemon-details ${
+                      isHovered ? "active" : ""
+                    }`}
+                  >
+                    {" "}
+                    <h3>Types</h3>
+                    {pokemon.types.map((item, i) => (
+                      <p key={i}>{item.type.name}</p>
+                    ))}
+                    <h3>Abilities</h3>
+                    {pokemon.abilities.map((item, i) => (
+                      <p key={i}>{item.ability.name}</p>
+                    ))}
+                    <h3>Stats</h3>
+                    {pokemon.stats.map((item, i) => (
+                      <p key={i}>
+                        {item.stat.name}: {item.base_stat}
+                      </p>
+                    ))}
+                  </div>
+                )}
+                {!isOnTeamPage && (
+                  <>
+                    <BtnDetails pokemonName={pokemon.name} />
+                    <button
+                      className="btn-remove"
+                      onClick={() => removeFromTeam(i)}
+                    >
+                      remove
+                    </button>
+                  </>
+                )}
               </div>
             </article>
           ))}
